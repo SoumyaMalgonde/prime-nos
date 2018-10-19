07c372948823c15201fcb39b17b4f1b901cd9986
ChandanSitlani <40667874+ChandanSitlani@users.noreply.github.com>
2018-10-19 16:42:32 +0530

    Create Finding prime nos.cpp

---
+++ Finding prime nos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<math.h>
+int isprime(int);
+
+
+ int main()
+ {
+ 	int n;
+ 	cin>>n;
+ 	for(int i=2;i<=n;i++)
+ 	if(isprime(n))
+ 	cout<<i<<endl;
+ 	
+ }
+
+int isprime(int n)
+{
+for(int i=0;i<pow(n,0.5);i++)
+if(n%i==0)
+return 0;
+return 1;
+}
